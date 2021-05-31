85321eca805bdf1c1430639a1b4d6efdf2cff2a7
yanglvfeng <yanglvfeng@croot.com>
2021-05-31 17:57:43 +0800

    update overtime-record-upload接口数据处理

--- src/routers/overtimeRecord/upload.js
+++ src/routers/overtimeRecord/upload.js
@@ -1,11 +1,13 @@
 const {API,tryExecute} = require("../../utils");
+const xlxs = require('xlsx');
 const {OvertimeRecordCollection} = require('../../data/models');
 
 async function upload(req,res,next){
     tryExecute(res,"上传OvertimeRecord数据",async ()=>{
-        // const buffer = req.files[0].buffer;
-        // const result = xlxs.read()
-        console.log(req.files)
+        const buffer = req.files[0].buffer;
+        const result = xlxs.read(buffer,{type:'buffer'})
+        console.log('result',result.Sheets.Sheet1)
+        // console.log(xlxs.utils.sheet_to_json(result.Sheets.Sheet1))
         API.success(res);
     })
 }
